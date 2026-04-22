a917771fe9b2361e61ef679f4e7ecb8ebe6486fc
Maryamqg1 <maryamqg1@gmail.com>
2026-04-22 16:24:00 +0100

    deleted duplicated files from works that are not needed

--- module.py
+++
@@ -1,6 +0,0 @@
-def myName(name):
-    '''
-    function is imported in the useModule.py file
-    '''
-
-    return f'Hello, i am {name}'
--- useModule.py
+++
@@ -1,4 +0,0 @@
-import module
-import datetime
-
-print(module.myName('maryam') + ' and the today is ' + str(datetime.date.today()))
